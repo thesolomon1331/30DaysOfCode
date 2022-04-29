c3ea9e64b2dd24da8a7644527be0e25b04dc628b
Solomon Raju G <53774710+thesolomon1331@users.noreply.github.com>
2022-04-29 23:35:52 +0530

    Insertion Sort
    Java program to sort the array using Insertion Sort technique.

---
+++ InsertionSort.java
@@ -0,0 +1,37 @@
+/*Java program to sort the array using "Insertion sort" technique
+ * Insertion sort: Take an element from an unsorted array and place it in its corresponding position in 
+ * the sorted part, and shift the elements accordingly.
+ * Time ComplexityL O(N^2).
+ * */
+public class InsertionSort
+{
+	static void Printarray(int arr[])
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(" "+arr[i]);
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		int arr[]= {8,1,10,5,4,9};
+		System.out.print("Values before sorting the array :");
+		Printarray(arr);
+		//Applying Insertion sort Technique
+		for(int i=0;i<arr.length;i++)
+		{
+			int current=arr[i];
+			int j=i-1;
+			while(j>=0 && arr[j]>current)
+			{
+				arr[j+1]=arr[j];
+				j--;
+			}
+			arr[j+1]=current;
+			
+		}
+		System.out.print("Values After sorting the array : ");
+		Printarray(arr);
+	}
+}
