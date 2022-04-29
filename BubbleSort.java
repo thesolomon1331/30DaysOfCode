5958d962627acb6a45e26da58e8c9d2399d222a4
Solomon Raju G <53774710+thesolomon1331@users.noreply.github.com>
2022-04-29 23:41:28 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,8 +1,7 @@
 /*Java Program to sort the give array of elements using Bubble sort technique
- * Bubble Sort: It is the simplest sorting algorithm, where current element is compared with its next
- * element if value is greater then Swapped.
+ Bubble Sort: It is the simplest sorting algorithm, where current element is compared with its next element if value is greater then Swapped.
  * Time complexity of this algorithm is O(N^2)
- * */
+ */
 public class BubbleSort
 {
 	public static void Printarray(int arr[])
@@ -18,14 +17,14 @@ public class BubbleSort
 		System.out.println("Values before Swapping: ");
 		Printarray(arr);
 		// Applying Bubble Sort technique....
-		for(int i=0;i<arr.length-1;i++)
+		for(int i=0;i<arr.length-1;i++) // outer for loop
 		{
-			for(int j=0;j<arr.length-i-1;j++)
+			for(int j=0;j<arr.length-i-1;j++)  // 	Inner for loop
 			{
 				if(arr[j]>arr[j+1])
 				{
 					int temp=arr[j];
-					arr[j]=arr[j+1];
+					arr[j]=arr[j+1];   // Swap
 					arr[j+1]=temp;
 				}
 			}
