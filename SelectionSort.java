d6a011263fb082f9adfacc2ae59919ebe992467c
Solomon Raju G <53774710+thesolomon1331@users.noreply.github.com>
2022-04-30 23:16:29 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,6 @@
 /*Java Program to sort the elements using Insertion sort technique.
 Insertion Sort: In this method the array will be divided into two parts i.e unsorted array and sorted array, and place it in its corresponding position in 
- the sorted part, and shift the elements accordingly.
+the sorted part, and shift the elements accordingly.
  * Time ComplexityL O(N^2).
 */
 public class SelectionSort
@@ -18,10 +18,10 @@ public class SelectionSort
 		System.out.print("Before Sorting the elements");
 		Printarray(arr);
 		// Selection Sort technique
-		for(int i=0;i<arr.length-1;i++)
+		for(int i=0;i<arr.length-1;i++)// Outer for loop
 		{
 			int smallest=i;
-			for(int j=i+1;j<arr.length;j++)
+			for(int j=i+1;j<arr.length;j++)// Inner for loop
 			{
 				if(arr[j]<arr[smallest])
 				{
